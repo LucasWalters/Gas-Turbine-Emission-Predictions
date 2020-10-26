cfee9412a4b8ba9ed4cc9845094b75f864635cd1
LucasWalters <lucas.eduard.walters@gmail.com>
2020-10-26 12:24:03 +0100

    Cleaned up the code and added bools to toggle specific functionality

--- part-2.py
+++ part-2.py
@@ -5,6 +5,14 @@ import pandas as pd
 import json
 import seaborn as sns
 
+
+print_data = False
+print_corr_matrix = True
+save_csv = False
+save_corr_matrices = False
+save_bar_chart = False
+
+
 # File naming and path
 data_folder = 'pp_gas_emission'
 file_prefix = 'gt_'
@@ -49,6 +57,7 @@ years = [ '2011', '2012', '2013', '2014', '2015' ]
 
 # Final result object
 result_data = {}
+# Object that will contain the correlation matrices
 correlation_matrices = {}
 # Dataframe that will contain all files combined
 total_df = None
@@ -66,93 +75,104 @@ for year in years:
     
     # Calculate stats and put it in the result under this year
     result_data[year] = calc_stats(file_df)
+    # Calculate correlation matrix and put it under this year
     correlation_matrices[year] = file_df.corr(method='spearman')
+    
 
 
 # Calculate stats of the total dataframe and put it in the result under 'All'
 result_data['All'] = calc_stats(total_df)
 correlation_matrices['All'] = total_df.corr(method='spearman')
-# Print the resulting object with nice formatting
-print(json.dumps(result_data, indent=4))
+
+if print_data:
+    # Print the resulting object with nice formatting
+    print(json.dumps(result_data, indent=4))
+
+if print_corr_matrix:
+    # Print correlation matrix of 2011
+    print(correlation_matrices['2011'])
 
 # Let's turn that into a multi indexed dataframe, should've probably done this at the start but only thought of it now so whatever
 # Get first index array - the years and 'All'
 years_and_all = np.array(list(result_data.keys()))
 # Get second index array - the variable names (AT, AP, AFDP, etc)
 variables = np.array(list(result_data['All'].keys()))
+
+statistics = np.array(list(variable_data.keys()))
 # Make multi index
 midx = pd.MultiIndex.from_product([years_and_all, variables])
 
 # Create the actual dataframe, the columns are the statistics names (mean, median, etc)
-df = pd.DataFrame(index = midx, columns = list(variable_data.keys()))
+df = pd.DataFrame(index = midx, columns = statistics)
 
 # Fill dataframe with data we got earlier
-for index in result_data:
+for index in years_and_all:
     for variable in result_data[index]:
         for statistic in result_data[index][variable]:
             value = result_data[index][variable][statistic]
             df[statistic][index][variable] = value
 
-# Print the start of it, we can now easily use this for the plots
-print(df.head())
+if print_data:
+    # Print the start of it, we can now easily use this for the plots
+    print(df.head())
 
-# Write yearly data to file
-for variable in variable_data.keys():
-    df[variable] = df[variable].astype(float)
-df.to_csv('data.csv', float_format='%.2f')
+if save_csv:
+    # Write yearly data to file
+    for variable in variable_data.keys():
+        df[variable] = df[variable].astype(float)
+    df.to_csv('data.csv', float_format='%.2f')
 
+if save_corr_matrices:
 # Write correlation matrices to file and plot them
-for index in correlation_matrices:
-    correlation_matrices[index].to_csv('correlation_'+str(index)+'.csv', float_format='%.2f')
+    for index in correlation_matrices:
+        correlation_matrices[index].to_csv('correlation_'+str(index)+'.csv', float_format='%.2f')
 
-    plt.figure(figsize=(8, 5.5))
-    sns.heatmap(correlation_matrices[index].round(2), annot=True, vmin=-1, vmax=1, center=0, cmap='coolwarm', square=True)
-    plt.yticks(rotation=0) 
-    plt.savefig('Correlations_'+str(index)+'.png',bbox_inches='tight')
-
-# TODO charts:
-
-# labels = result_data['All'].keys()
+        plt.figure(figsize=(8, 5.5))
+        sns.heatmap(correlation_matrices[index].round(2), annot=True, vmin=-1, vmax=1, center=0, cmap='coolwarm', square=True)
+        plt.yticks(rotation=0) 
+        plt.savefig('Correlations_'+str(index)+'.png',bbox_inches='tight')
 
 
 
-# labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-# men_means = [20, 34, 30, 35, 27]
-# women_means = [25, 32, 34, 20, 25]
+if save_bar_chart:
+    # Prepare data for bar charts
+    df_swapped = df.swaplevel(0, 1, axis=0)
+    labels = years_and_all
+    fig, ax = plt.subplots()
+    x = []  # the label locations
+    width = 0.1  # the width of the bars
+    label_width = width * len(variables)
+    for i in range(len(labels)):
+        x.append(label_width * i)
+    x = np.array(x)
+    print(x)
 
-# x = np.arange(len(labels))  # the label locations
-# width = 0.35  # the width of the bars
-
-# fig, ax = plt.subplots()
-# rects1 = ax.bar(x - width/2, men_means, width, label='Men')
-# rects2 = ax.bar(x + width/2, women_means, width, label='Women')
-
-# Add some text for labels, title and custom x-axis tick labels, etc.
-# ax.set_ylabel('Scores')
-# ax.set_title('Scores by group and gender')
-# ax.set_xticks(x)
-# ax.set_xticklabels(labels)
-# ax.legend()
-
-
-# def autolabel(rects):
-    # """Attach a text label above each bar in *rects*, displaying its height."""
-    # for rect in rects:
-        # height = rect.get_height()
-        # ax.annotate('{}'.format(height),
-                    # xy=(rect.get_x() + rect.get_width() / 2, height),
-                    # xytext=(0, 3),  # 3 points vertical offset
-                    # textcoords="offset points",
-                    # ha='center', va='bottom')
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
 
 
-# autolabel(rects1)
-# autolabel(rects2)
+    for i, variable in enumerate(variables):
+        variable_means = list(df_swapped['mean'][variable])
+        rects = ax.bar(x + i * width, variable_means, width, label=variable, align='edge')
+        autolabel(rects)
 
-# fig.tight_layout()
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Mean')
+    ax.set_title('Mean of variables by year')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
 
-# plt.show()
+    fig.tight_layout()
 
+    plt.show()
     
 
 
