f6a0db5d00c8414731b541dfd4ec0a1a0156f133
Lucas Walters <lucas.eduard.walters@gmail.com>
2020-10-28 21:31:51 +0100

    Fixed the blocks issue

--- part-3.py
+++ part-3.py
@@ -58,7 +58,7 @@ def apply_linear_regression(training_data, training_target, test_data, test_targ
 
     # Predict on test data
     test_pred = regr.predict(test_data)
-    
+
     (SC, MAE, R2) = compute_performance(test_pred, test_target)
     print_performance(SC, MAE, R2)
     return ((SC, MAE, R2), pd.DataFrame(test_pred, columns=[target_column]))
@@ -79,7 +79,7 @@ def compute_val_and_test_performance(train_data, train_out, val_data, val_out, t
 def phase2(baseline, train_data, train_out, val_data, val_out, test_data, test_out):
     #feature_comb = find_features(baseline[0], input_variable_names, train_data.copy(), train_out, val_data.copy(), val_out)
     feature_comb = [('AH', 'TAT'), ('AFDP', 'TAT'), ('GTEP', 'TIT'), ('TEY', 'CDP')]
-    
+
     train_data = apply_engineered_features(train_data, feature_comb)
     val_data = apply_engineered_features(val_data, feature_comb)
     test_data = apply_engineered_features(test_data, feature_comb)
@@ -100,22 +100,22 @@ def phase2(baseline, train_data, train_out, val_data, val_out, test_data, test_o
     if apply_pca:
         # Make an instance of the Model. .95 means the minimum number of principal components such that 95% of the variance is retained.
         pca = PCA(.95)
-        
+
         pca.fit(train_reduced)
-        
+
         train_pca = pca.transform(train_reduced)
         val_pca = pca.transform(val_reduced)
         test_pca = pca.transform(test_reduced)
-        
+
         train_pca_df = pd.DataFrame(train_pca, columns = ['pca_' + str(x) for x in range(len(train_pca[0]))])
         val_pca_df = pd.DataFrame(val_pca, columns = ['pca_' + str(x) for x in range(len(val_pca[0]))])
         test_pca_df = pd.DataFrame(test_pca, columns = ['pca_' + str(x) for x in range(len(test_pca[0]))])
-        
+
         train_merged = pd.concat([train_reduced, train_pca_df], axis = 1)
         val_merged = pd.concat([val_reduced, val_pca_df], axis = 1)
         test_merged = pd.concat([test_reduced, test_pca_df], axis = 1)
-        
-        
+
+
         print("On validation with PCA")
         apply_linear_regression(train_merged, train_out, val_merged, val_out)
         print("On test with PCA")
@@ -123,15 +123,19 @@ def phase2(baseline, train_data, train_out, val_data, val_out, test_data, test_o
 
 def predict_blocks(block_train_data, blocks, variable_columns, target_columns):
     print("Testing block", 10-len(blocks))
-    result = apply_linear_regression(block_train_data[variable_columns], block_train_data[target_columns], blocks[0][variable_columns], blocks[0][target_columns]) 
+    result = apply_linear_regression(block_train_data[variable_columns], block_train_data[target_columns], blocks[0][variable_columns], blocks[0][target_columns])[1]
     blocks[0][target_columns] = result
     if len(blocks) > 1:
-        predict_blocks(pd.concat([block_train_data, blocks[0]]), np.delete(blocks, 0), variable_columns, target_columns)
+        predict_blocks(pd.concat([block_train_data, blocks[0]]), blocks[1:], variable_columns, target_columns)
 
 def phase3(train_data, data_2013, data_2014, data_2015, variable_names, target_columns):
     val_blocks = np.array_split(data_2013, 10)
     val_blocks = [df.reset_index(drop = True) for df in val_blocks]
-    test_blocks = np.append(np.array_split(data_2014, 10), np.array_split(data_2015, 10))
+    test_blocks = []
+    for block in np.array_split(data_2014, 10):
+        test_blocks.append(block)
+    for block in np.array_split(data_2015, 10):
+        test_blocks.append(block)
     test_blocks = [df.reset_index(drop = True) for df in test_blocks]
     predict_blocks(train_data.reset_index(drop = True), val_blocks, variable_names, target_columns)
 
