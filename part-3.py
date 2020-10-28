d42a3066058b71cd7c6075f1aa76a340bbcc76e3
Julian Thijssen <Nimthora@gmail.com>
2020-10-28 16:17:19 +0100

    Clean up code a bit more

--- part-3.py
+++ part-3.py
@@ -6,6 +6,7 @@ from sklearn import linear_model
 from sklearn.metrics import mean_absolute_error
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from utils import *
 
 apply_pca = True
 
@@ -46,31 +47,20 @@ for year in years:
     # Add to total dataframe
     total_df[year] = file_df
 
-training_df = pd.concat([total_df['2011'], total_df['2012']])
-validation_df = total_df['2013']
+train_df = pd.concat([total_df['2011'], total_df['2012']])
+val_df = total_df['2013']
 test_df = pd.concat([total_df['2014'], total_df['2015']])
 
-def z_normalize(training_df, validation_df, test_df):
-    # Scale the data on unit scale (mean = 0, variance = 1)
-    scaler = StandardScaler()
-    # Fit on training set only.
-    scaler.fit(training_df[input_variable_names])
-    # Apply transform to both the training set and the test set.
-    train_scaled = pd.DataFrame(scaler.transform(training_df[input_variable_names]), columns = input_variable_names)
-    val_scaled = pd.DataFrame(scaler.transform(validation_df[input_variable_names]), columns = input_variable_names)
-    test_scaled = pd.DataFrame(scaler.transform(test_df[input_variable_names]), columns = input_variable_names)
-    return (train_scaled, training_df['NOX'], val_scaled, validation_df['NOX'], test_scaled, test_df['NOX'])
+# Split the data into input and output data
+train_data = train_df[input_variable_names]
+train_out = train_df['NOX']
+val_data = val_df[input_variable_names]
+val_out = val_df['NOX']
+test_data = test_df[input_variable_names]
+test_out = test_df['NOX']
 
-def compute_performance(name, pred, observed):
-    correlation_df = pd.DataFrame({'NOXa': pred, 'NOXb': observed}, columns=['NOXa', 'NOXb'])
-    NOX_correlation = correlation_df.corr(method='spearman').iloc[1][0]
-    NOX_mae = mean_absolute_error(observed, pred)
-    NOX_r2 = correlation_df.corr(method='pearson').iloc[1][0] ** 2
-
-    print(name + " NOX Spearman Correlation: " + str(NOX_correlation))
-    print(name + " NOX Mean absolute error: " + str(NOX_mae))
-    print(name + " NOX R^2: " + str(NOX_r2))
-
+# Normalize the data
+(train_data, val_data, test_data) = z_normalize(train_data, val_data, test_data, input_variable_names)
 
 def apply_linear_regression(training_data, training_target, test_data, test_target, prefix = "[TEST]"):
     # Regress on the training data
@@ -79,8 +69,9 @@ def apply_linear_regression(training_data, training_target, test_data, test_targ
 
     # Predict on test data
     test_pred = regr.predict(test_data)
-
-    compute_performance(prefix, test_pred, test_target)
+    
+    (SC, MAE, R2) = compute_performance(prefix, test_pred, test_target)
+    print_performance(SC, MAE, R2)
 
 def phase1(training_df, training_t, validation_df, validation_t, test_df, test_t):
     ### Predict NOX values for the validation data
@@ -90,14 +81,16 @@ def phase1(training_df, training_t, validation_df, validation_t, test_df, test_t
     X = pd.concat([training_df, validation_df])
     Y = pd.concat([training_t, validation_t])
 
-    apply_linear_regression(X, Y, test_df, test_t, "[TEST]")
+    #apply_linear_regression(X, Y, test_df, test_t, "[TEST]")
 
-def phase2(training_df, training_t, validation_df, validation_t, test_df, test_t):
-    train_reduced = training_df[reduced_input_variable_names]
-    test_reduced = test_df[reduced_input_variable_names]
-    validation_reduced = validation_df[reduced_input_variable_names]
+def phase2(train_data, train_out, val_data, val_out, test_data, test_out):
+    #find_features(reduced_input_variable_names, train_data.copy(), train_out, val_data.copy(), val_out)
     
-    apply_linear_regression(train_reduced, training_t, validation_reduced, validation_t, "[VAL-REDUCED]")
+    train_reduced = train_data[reduced_input_variable_names]
+    val_reduced = val_data[reduced_input_variable_names]
+    test_reduced = test_data[reduced_input_variable_names]
+
+    apply_linear_regression(train_reduced, train_out, val_reduced, val_out, "[VAL-REDUCED]")
 
     if apply_pca:
         # Make an instance of the Model. .95 means the minimum number of principal components such that 95% of the variance is retained.
@@ -106,23 +99,19 @@ def phase2(training_df, training_t, validation_df, validation_t, test_df, test_t
         pca.fit(train_reduced)
         
         train_pca = pca.transform(train_reduced)
+        val_pca = pca.transform(val_reduced)
         test_pca = pca.transform(test_reduced)
-        validation_pca = pca.transform(validation_reduced)
         
         train_pca_df = pd.DataFrame(train_pca, columns = ['pca_' + str(x) for x in range(len(train_pca[0]))])
+        val_pca_df = pd.DataFrame(val_pca, columns = ['pca_' + str(x) for x in range(len(val_pca[0]))])
         test_pca_df = pd.DataFrame(test_pca, columns = ['pca_' + str(x) for x in range(len(test_pca[0]))])
-        validation_pca_df = pd.DataFrame(validation_pca, columns = ['pca_' + str(x) for x in range(len(validation_pca[0]))])
         
         train_merged = pd.concat([train_reduced, train_pca_df], axis = 1)
+        val_merged = pd.concat([val_reduced, val_pca_df], axis = 1)
         test_merged = pd.concat([test_reduced, test_pca_df], axis = 1)
-        validation_merged = pd.concat([validation_reduced, validation_pca_df], axis = 1)
         
-        apply_linear_regression(train_merged, training_t, validation_merged, validation_t, "[VAL-PCA]")
-        apply_linear_regression(train_merged, training_t, test_merged, test_t, "[TEST-PCA]")
+        apply_linear_regression(train_merged, train_out, val_merged, val_out, "[VAL-PCA]")
+        apply_linear_regression(train_merged, train_out, test_merged, test_out, "[TEST-PCA]")
 
-        
-
-(training_df, training_t, validation_df, validation_t, test_df, test_t) = z_normalize(training_df, validation_df, test_df)
-phase1(training_df, training_t, validation_df, validation_t, test_df, test_t)
-phase2(training_df, training_t, validation_df, validation_t, test_df, test_t)
-
+#phase1(training_df, training_t, validation_df, validation_t, test_df, test_t)
+phase2(train_data, train_out, val_data, val_out, test_data, test_out)
